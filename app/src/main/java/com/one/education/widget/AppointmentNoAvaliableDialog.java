8fc474d0e2204793b7014af8eadb8c562a9044bf
lzs <linzhisheng@funo.com.cn>
2021-03-26 08:53:02 +0800

    上传uikit相应资源

---
+++ app/src/main/java/com/one/education/widget/AppointmentNoAvaliableDialog.java
@@ -0,0 +1,20 @@
+package com.one.education.widget;
+
+import android.content.Context;
+import android.widget.TextView;
+
+import com.one.education.education.R;
+
+public class AppointmentNoAvaliableDialog extends CenterDialog {
+    private TextView mMessageTv;
+    public AppointmentNoAvaliableDialog(Context context) {
+        super(context, R.layout.appointment_no_aviable_dialog);
+        mMessageTv = findViewById(R.id.message_tv);
+        setCanceledOnTouchOutside(false);
+        findViewById(R.id.let_ok).setOnClickListener(v -> dismiss());
+    }
+
+    public void setMessageTv(String message) {
+        mMessageTv.setText(message);
+    }
+}
