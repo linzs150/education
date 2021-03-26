f19e5aa670e3e28e98381841932891071794f16c
lzs <linzhisheng@funo.com.cn>
2021-03-26 13:10:23 +0800

    上传uikit相应资源

---
+++ mylibrary/src/main/java/com/one/mylibrary/ConstantGlobal.java
@@ -0,0 +1,17 @@
+package com.one.mylibrary;
+
+public interface ConstantGlobal {
+    String LOCALE_LANGUAGE = "locale_language";
+    String LOCALE_COUNTRY = "locale_country";
+    public enum LanguageType{
+        CHINESE,
+        EN,
+        HK
+    }
+
+    public static class Net {
+
+        //        public static final String URL = "http://121.idaguo.com/api";//测试环境
+        public static final String URL = "https://www.newtontutor.hk/api";//生产环境
+    }
+}
